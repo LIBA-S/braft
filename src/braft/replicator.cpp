471f60ad9767963251527c1ead3be2cdd1b18903
vincebai <vincebai@tencent.com>
2023-06-15 20:32:57 +0800

    braft with auth
    --story=885073715

--- src/braft/replicator.cpp
+++ src/braft/replicator.cpp
@@ -21,6 +21,7 @@
 #include <butil/time.h>                          // butil::gettimeofday_us
 #include <brpc/controller.h>                     // brpc::Controller
 #include <brpc/reloadable_flags.h>               // BRPC_VALIDATE_GFLAG
+#include "braft/auth.h"
 #include "braft/replicator.h"
 #include "braft/node.h"                          // NodeImpl
 #include "braft/ballot_box.h"                    // BallotBox 
@@ -113,6 +114,9 @@ int Replicator::start(const ReplicatorOptions& options, ReplicatorId *id) {
     brpc::ChannelOptions channel_opt;
     //channel_opt.connect_timeout_ms = *options.heartbeat_timeout_ms;
     channel_opt.timeout_ms = -1; // We don't need RPC timeout
+    if (g_braft_auth_getter) {
+        channel_opt.auth = g_braft_auth_getter();
+    }
     if (r->_sending_channel.Init(options.peer_id.addr, &channel_opt) != 0) {
         LOG(ERROR) << "Fail to init sending channel"
                    << ", group " << options.group_id;
